cbd2d24044292104ddfd071c08fe8154aaa2d3f6
Reyna <canche26rey@gmail.com>
2023-07-04 18:12:24 -0600

    Función onClick

--- 03-MiniProyecto-React-Issues/src/App.jsx
+++ 03-MiniProyecto-React-Issues/src/App.jsx
@@ -1,12 +1,61 @@
 import Search from '../src/Components/SearchBar'
+import { useState, useEffect } from 'react'
 import './App.css'
+import DataTable from 'react-data-table-component'
 
 const App = () => {
-  return (
-    <div>App
-      <Search/>
-    </div>
+  const [issues, setIssues] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
+  const URL = 'https://api.github.com/repos/facebook/react/issues'
+
+  const showData = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    console.log(data)
+    setIssues(data)
+  }
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredData = issues.filter((issue) => {
+    return issue.title.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  useEffect(() => {
+    showData()
+  }, [])
+
+  const handleTitleClick = (issue) => {
+    window.open(issue.html_url, '_blank')
+  }
+
+  const columns = [
+    {
+      name: 'ID',
+      selector: 'id'
+    },
+    {
+      name: 'NAME',
+      selector: 'user.login'
+    },
+    {
+      name: 'TITULO',
+      selector: 'title',
+      cell: (row) => <span onClick={() => handleTitleClick(row)}>{row.title}</span>
+    }
+  ]
+
+  return (
+    <div>
+      <div className='Search-bar'>
+        <Search handleSearchChange={handleSearch} />
+      </div>
+      <h1>Tabla de issues</h1>
+      <DataTable columns={columns} data={filteredData} />
+    </div>
   )
 }
 
--- 03-MiniProyecto-React-Issues/src/Components/Elemento.jsx
+++ 03-MiniProyecto-React-Issues/src/Components/Elemento.jsx
@@ -1,25 +1,14 @@
 
-
-function Elemento= ({ id, title,  url, nombre }) => { 
+const Elemento = ({ id, title,  url, nombre }) => {
   return
    (
-    <div className='col-md-3 my-3 d-flex align-items-stretch'>
-        <div className='card'>
-          <a href={`/product/${id}`}>
-            <div className='image-container'>
+        <div>
+          <h5>{id}</h5>
+          <a href={url}>{title}</a>
+          <h2>{nombre}</h2>
+              
+              
             </div>
-          </a>
-          <div className='card-body d-flex flex-column justify-content-between'>
-            <div>
-              <h5 className='card-title' onClick={url}>{title}</h5>
-            </div>
-  
-            <div className='mx-auto mt-3'>
-              <p className='card-nombre'>${nombre}</p>
-            </div>
-          </div>
-        </div>
-      </div>
   )
 }
 export default Elemento
